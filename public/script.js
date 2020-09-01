2d1aacf1a16606f2c65483ec41552b1117daa683
Kingofthesun <48035481+Kingofthesun@users.noreply.github.com>
2020-09-01 08:21:39 -0600

    finishing table

--- public/script.js
+++ public/script.js
@@ -9,34 +9,46 @@ const graph = canvas.getContext('2d');
 canvas.width=600;
 canvas.height=400;
 
+let dogNum;
+let catNum;
+let dogCatNum;
+let neitherDogCatNum;
+let moviesNum;
+let gamingNum;
+let readingNum;
+let sportsNum;
+let otherNum;
+let fraculatorNum;
+let weatherNum;
+let thisAssignmentNum;
+let otherAssignmentNum;
+
+const url = 'http://localhost:3000/api'
+
 fetch(url)
     .then(response => response.json())
     .then(data => {
         console.log(data);
         // The "data" variable has the data from the API. I've filled
         // in your variables with it here:
-        const dogNum = data[0][0];
-        const catNum = data[0][1];
-        const dogCatNum = data[0][2];
-        const neitherDogCatNum = data[0][3];
-        
-        const moviesNum = data[1][0];
-        const gamingNum = data[1][1];
-        const readingNum = data[1][2];
-        const sportsNum = data[1][3];
-        const otherNum = data[1][4];
-        
-        const fraculatorNum = data[2][0];
-        const weatherNum = data[2][1];
-        const thisAssignmentNum = data[2][2];
-        const otherAssignmentNum = data[2][3];
+        dogNum = data[0][0];
+        catNum = data[0][1];
+        dogCatNum = data[0][2];
+        neitherDogCatNum = data[0][3];
+        moviesNum = data[1][0];
+        gamingNum = data[1][1];
+        readingNum = data[1][2];
+        sportsNum = data[1][3];
+        otherNum = data[1][4];
+        fraculatorNum = data[2][0];
+        weatherNum = data[2][1];
+        thisAssignmentNum = data[2][2];
+        otherAssignmentNum = data[2][3];
 
         // All the code for making the graph needs to go in here.
         
-    });
 
 
-//these are currently fake numbers for testing purposes based on a total of 585 users
 
 const dogGraph = (dogNum*100)/(dogNum+catNum+dogCatNum+neitherDogCatNum)
 const catGraph = (catNum*100)/(dogNum+catNum+dogCatNum+neitherDogCatNum)
@@ -47,9 +59,11 @@ const neitherDogCatGraph = (neitherDogCatNum*100)/(dogNum+catNum+dogCatNum+neith
 graph.fillStyle = '#222299';
 graph.fillRect (0, (300-dogGraph*3), 40, dogGraph*3);
 
+
 graph.fillStyle = '#1eb4a6';
 graph.font = '14px Arial';
 graph.fillText('Dog', 10, 320);
+graph.fillText(dogNum, 10, (300-dogGraph*3)-10);
 
 
 graph.fillStyle = '#BB2222';
@@ -58,6 +72,7 @@ graph.fillRect (40, (300-catGraph*3), 40, catGraph*3);
 graph.fillStyle = '#1eb4a6';
 graph.font = '14px Arial';
 graph.fillText('Cat', 50, 320);
+graph.fillText(catNum, 50, (300-catGraph*3)-10);
 
 
 graph.fillStyle = '#6F225E';
@@ -66,6 +81,7 @@ graph.fillRect (80, (300-dogCatGraph*3), 40, dogCatGraph*3);
 graph.fillStyle = '#1eb4a6';
 graph.font = '14px Arial';
 graph.fillText('Both', 85, 320);
+graph.fillText(dogCatNum, 90, (300-dogCatGraph*3)-10);
 
 
 graph.fillStyle = '#844400';
@@ -74,7 +90,7 @@ graph.fillRect (120, (300-neitherDogCatGraph*3), 40, neitherDogCatGraph*3);
 graph.fillStyle = '#1eb4a6';
 graph.font = '14px Arial';
 graph.fillText('Neither', 120, 320);
-
+graph.fillText(neitherDogCatNum, 130, (300-neitherDogCatGraph*3)-10);
 
 
 
@@ -101,6 +117,8 @@ graph.fillRect (200, (300-movieGraph*3), 40, movieGraph*3);
 graph.fillStyle = '#1eb4a6';
 graph.font = '14px Arial';
 graph.fillText('Movies', 199, 320);
+graph.fillText(moviesNum, 210, (300-movieGraph*3)-10);
+
 
 
 graph.fillStyle = '#4682B4';
@@ -109,6 +127,7 @@ graph.fillRect (240, (300-gamingGraph*3), 40, gamingGraph*3);
 graph.fillStyle = '#1eb4a6';
 graph.font = '14px Arial';
 graph.fillText('Gaming', 235, 340);
+graph.fillText(gamingNum, 250, (300-gamingGraph*3)-10);
 
 
 graph.fillStyle = '#E8F48C';
@@ -117,6 +136,7 @@ graph.fillRect (280, (300-readingGraph*3), 40, readingGraph*3);
 graph.fillStyle = '#1eb4a6';
 graph.font = '14px Arial';
 graph.fillText('Reading', 277, 320);
+graph.fillText(readingNum, 290, (300-readingGraph*3)-10);
 
 
 graph.fillStyle = '#228B22';
@@ -125,6 +145,7 @@ graph.fillRect (320, (300-sportsGraph*3), 40, sportsGraph*3);
 graph.fillStyle = '#1eb4a6';
 graph.font = '14px Arial';
 graph.fillText('Sports', 325, 340);
+graph.fillText(sportsNum, 330, (300-sportsGraph*3)-10);
 
 
 graph.fillStyle = '#844400';
@@ -133,6 +154,7 @@ graph.fillRect (360, (300-otherGraph*3), 40, otherGraph*3);
 graph.fillStyle = '#1eb4a6';
 graph.font = '14px Arial';
 graph.fillText('Other', 365, 320);
+graph.fillText(otherNum, 370, (300-otherGraph*3)-10);
 
 
 graph.fillStyle = '#1eb4a6';
@@ -158,6 +180,7 @@ graph.fillRect (440, (300-fraculatorGraph*3), 40, fraculatorGraph*3);
 graph.fillStyle = '#1eb4a6';
 graph.font = '14px Arial';
 graph.fillText('Fraculator', 435, 320);
+graph.fillText(fraculatorNum, 450, (300-fraculatorGraph*3)-10);
 
 
 graph.fillStyle = '#87CEEB';
@@ -166,6 +189,7 @@ graph.fillRect (480, (300-weatherGraph*3), 40, weatherGraph*3);
 graph.fillStyle = '#1eb4a6';
 graph.font = '14px Arial';
 graph.fillText('Weather API', 465, 340);
+graph.fillText(weatherNum, 490, (300-weatherGraph*3)-10);
 
 
 graph.fillStyle = '#ff9f1c';
@@ -174,6 +198,8 @@ graph.fillRect (520, (300-thisAssignmentGraph*3), 40, thisAssignmentGraph*3);
 graph.fillStyle = '#1eb4a6';
 graph.font = '14px Arial';
 graph.fillText('This One', 515, 320);
+graph.fillText(thisAssignmentNum, 530, (300-thisAssignmentGraph*3)-10);
+
 
 graph.fillStyle = '#844400';
 graph.fillRect (560, (300-otherAssignmentGraph*3), 40, otherAssignmentGraph*3);
@@ -181,9 +207,12 @@ graph.fillRect (560, (300-otherAssignmentGraph*3), 40, otherAssignmentGraph*3);
 graph.fillStyle = '#1eb4a6';
 graph.font = '14px Arial';
 graph.fillText('Other', 565, 340);
+graph.fillText(otherAssignmentNum, 570, (300-otherAssignmentGraph*3)-10);
 
 
 graph.fillStyle = '#1eb4a6';
 graph.font = '24px Arial';
 graph.fillText('Hardest', 470, 365);
 graph.fillText('Assignment?', 445, 385);
+
+});
